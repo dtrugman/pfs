846fb6eadd73d510d0a32ca44f98f9c68116fe1e
Felix Janda <felix.janda@posteo.de>
2024-04-24 10:13:10 -0400

    src/mem.cpp: replace the deprecated preadv64 by preadv
    This fixes a compilation error with musl libc.

--- src/mem.cpp
+++ src/mem.cpp
@@ -51,7 +51,7 @@ std::vector<uint8_t> mem::read(loff_t offset, size_t bytes)
 
     struct iovec iov = {&buffer[0], buffer.size()};
 
-    ssize_t bytes_read = preadv64(_fd, &iov, 1, offset);
+    ssize_t bytes_read = preadv(_fd, &iov, 1, offset);
     if (bytes_read == -1)
     {
         throw std::system_error(errno, std::system_category(),
